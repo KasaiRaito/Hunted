19e00bb5238df842f5b702835d1312c77d01f678
Sykher <jogreach.22@gmail.com>
2025-11-11 12:23:55 -0600

    Finish Gun Fix
    >Added Inputs for Aim Shoot

--- Source/Hunted/Private/HuntedGameplayTags.cpp
+++ Source/Hunted/Private/HuntedGameplayTags.cpp
@@ -5,16 +5,19 @@
 
 namespace  HuntedGameplayTags
 {
-	/* Input Tags */
+/* Input Tags */
+	/* Movement */
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Sneak, "InputTag.Sneak");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Sprint, "InputTag.Sprint");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Crouch, "InputTag.Crouch");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Snap, "InputTag.Snap");
-
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Echo, "InputTag.Echo");
-	
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+	/* Abilities */
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Echo, "InputTag.Echo");
+	/* Combat */
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Aim, "InputTag.Aim");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Shoot, "InputTag.Shoot");
 
 	/* Equip / Unequip */
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Equip_BasicKnife, "InputTag.Equip.BasicKnife");
