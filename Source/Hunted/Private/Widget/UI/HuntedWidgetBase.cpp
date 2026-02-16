1508e5987256bf00aa9e2e5f43ef2885e9e5dfb5
KasaiRaito <eduardo.huerta@saei.mx>
2026-02-16 00:52:11 -0600

    Listen & Send Event Function Implemented to WBP

--- Source/Hunted/Private/Widget/UI/HuntedWidgetBase.cpp
+++ Source/Hunted/Private/Widget/UI/HuntedWidgetBase.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Widget/UI/HuntedWidgetBase.h"
+
+#include "Components/UI/EnemyUIComponent.h"
 #include "Interfaces/PawnUIInterface.h"
 
 void UHuntedWidgetBase::NativeOnInitialized()
@@ -12,7 +14,21 @@ void UHuntedWidgetBase::NativeOnInitialized()
 	{
 		if (UPlayerUIComponent* PlayerUIComponent = PawnUIInterface->GetPlayerUIComponent())
 		{
-			BP_OnOwningHeroUIComponentInitialized(PlayerUIComponent);
+			BP_OnOwningPlayerUIComponentInitialized(PlayerUIComponent);
 		}
 	}
+}
+
+void UHuntedWidgetBase::InitEnemyCreateWidget(AActor* OwningEnemyActor)
+{
+#if WITH_EDITOR 
+	if (IPawnUIInterface* PawnUIInterface = Cast<IPawnUIInterface>(OwningEnemyActor))
+	{
+		UEnemyUIComponent* EnemyUIComponent = PawnUIInterface->GetEnemyUIComponent();
+		
+		checkf(EnemyUIComponent, TEXT("Failed to extract an EnemyUIComponent from %s"), *OwningEnemyActor->GetActorNameOrLabel());
+		
+		BP_OnOwningEnemyUIComponentInitialized(EnemyUIComponent);
+	}
+#endif
 }
