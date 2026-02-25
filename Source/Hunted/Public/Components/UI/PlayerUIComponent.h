1f2b6acd2924edbc043741899f215c4ba922c6ab
KasaiRaito <eduardo.huerta@saei.mx>
2026-02-24 22:42:43 -0600

    ShowMagBulletCount

--- Source/Hunted/Public/Components/UI/PlayerUIComponent.h
+++ Source/Hunted/Public/Components/UI/PlayerUIComponent.h
@@ -7,6 +7,8 @@
 #include "PlayerUIComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnBulletMagCountChangedDelegate, int, ShotCount, bool, IsMax);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnBulletBagCountChangedDelegate, int, ShotCount, bool, IsMax);
 
 /**
  * 
@@ -22,4 +24,10 @@ public:
 	
 	UPROPERTY(BlueprintCallable, BlueprintAssignable)
 	FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
+	
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnBulletMagCountChangedDelegate OnBulletMagCountChanged;
+	
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnBulletBagCountChangedDelegate OnBulletBagCountChanged;
 };
