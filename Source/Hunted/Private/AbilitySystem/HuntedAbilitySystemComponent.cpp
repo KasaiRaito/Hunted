0ec0c09679a886fdc2de5a51bb51b5350db3f522
KasaiRaito <eduardo.huerta@saei.mx>
2026-03-08 21:34:27 -0600

    EnemyRotate

--- Source/Hunted/Private/AbilitySystem/HuntedAbilitySystemComponent.cpp
+++ Source/Hunted/Private/AbilitySystem/HuntedAbilitySystemComponent.cpp
@@ -71,3 +71,26 @@ void UHuntedAbilitySystemComponent::RemoveGrantedPlayerWeaponAbilities(
 	
 	InSpecHandlesToRemove.Empty();
 }
+
+bool UHuntedAbilitySystemComponent::TryActivateAbilityByTag(FGameplayTag AbilityTagToActivate)
+{
+	check(AbilityTagToActivate.IsValid());
+	
+	TArray<FGameplayAbilitySpec*> FoundAbilitySpecs;
+	GetActivatableGameplayAbilitySpecsByAllMatchingTags(AbilityTagToActivate.GetSingleTagContainer(), FoundAbilitySpecs);
+	
+	if (FoundAbilitySpecs.IsEmpty())
+	{
+		const int32 RandomAbilityIndex = FMath::RandRange(0, FoundAbilitySpecs.Num() - 1);
+		FGameplayAbilitySpec* SpecToActivate = FoundAbilitySpecs[RandomAbilityIndex];
+		
+		check (SpecToActivate);
+		
+		if (!SpecToActivate->IsActive())
+		{
+			return TryActivateAbility(SpecToActivate->Handle);
+		}
+	}
+	
+	return false;
+}
